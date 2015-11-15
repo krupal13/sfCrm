ddedf8fa20f7ee6aec76d3cde81ef89a1eb19735
krupal13 <raf.krupinski@gmail.com>
2015-11-15 12:23:16 +0100

    Add navigation to base.html.twig

--- src/AppBundle/Controller/AgentController.php
+++ src/AppBundle/Controller/AgentController.php
@@ -1,6 +1,4 @@
-<?php
-
-namespace AppBundle\Controller;
+<?php namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -10,25 +8,27 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 /**
  * Class AgentController
  */
-class AgentController extends Controller {
-
+class AgentController extends Controller
+{
     /**
-     * @Route("/agent/list", name="agent_list")
+     * @Route("/agent/list", name="agents_list")
      * @Security("has_role('ROLE_MANAGER')")
      */
-    public function listAction(Request $request) {
-        $agents = $this->getDoctrine()
-                ->getRepository('AppBundle:UserDetailsAgent')
-                ->getAgents($this->getUser(), true);
-
-        $paginator = $this->get('knp_paginator');
+    public function listAction(Request $request)
+    {
+        $agents = $this->getDoctrine()->getManager()
+            ->getRepository('AppBundle:UserDetailsAgent')
+            ->getAgents($this->getUser(), true);
+        
+        $paginator  = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
-                $agents, $request->query->getInt('page', 1)/* page number */, 10/* limit per page */
+            $agents,
+            $request->query->getInt('page', 1)
         );
-
+        
+        
         return $this->render('agent/list.html.twig', [
-                    'agents' => $pagination
+            'agents' => $pagination,
         ]);
     }
-
 }
